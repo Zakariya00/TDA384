0229db91470a061033cf08279d887f4f139f16e4
zakariya00 <zakaria415.zo@gmail.com>
2022-09-05 13:05:57 +0200

    -2

--- Lab1/trainspotting/src/Train.java
+++ Lab1/trainspotting/src/Train.java
@@ -1,3 +1,4 @@
+import TSim.SensorEvent;
 import TSim.TSimInterface;
 
 public class Train extends Thread {
@@ -15,10 +16,18 @@ public class Train extends Thread {
     public void run () {
         try {
             tsi.setSpeed(this.id, this.speed);
+            //while (true)
+            //  processTrain (tsi.getSensor(this.id));
         }
         catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
         }
     }
+
+
+    public static void processTrain (SensorEvent event) {
+        throw new UnsupportedOperationException ("Not implemented, yet");
+    }
+
 }
