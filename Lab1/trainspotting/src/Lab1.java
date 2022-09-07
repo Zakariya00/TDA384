cb257f50aa6bc908ee49f1409cbe63e09ff69029
zakariya00 <zakaria415.zo@gmail.com>
2022-09-07 04:39:53 +0200

    Train can now choose station, stop, wait, reverse (Bugs!)

--- Lab1/trainspotting/src/Lab1.java
+++ Lab1/trainspotting/src/Lab1.java
@@ -1,17 +1,25 @@
 import TSim.*;
 
+import java.util.HashMap;
+import java.util.concurrent.Semaphore;
+
 public class Lab1 {
+  private HashMap<String, Semaphore> semaphores = new HashMap<>();
 
   public Lab1(int speed1, int speed2) {
     TSimInterface tsi = TSimInterface.getInstance();
+    putSemaphores();
 
     try {
 
-      Train train1 = new Train(1, speed1);
-      Train train2 = new Train(2, speed2);
+      Train train1 = new Train(1, speed1, semaphores, "stat_A1");
+      Train train2 = new Train(2, speed2, semaphores, "stat_B1");
 
       train1.start();
       train2.start();
+
+      tsi.setSwitch(17,7,0);
+      tsi.setSwitch(15,9,0);
 
       //tsi.setSpeed(2,speed2);
     }
@@ -19,5 +27,11 @@ public class Lab1 {
       e.printStackTrace();    // or only e.getMessage() for the error
       System.exit(1);
     }
+  }
+  public void putSemaphores() {
+    semaphores.put("stat_A1", new Semaphore(1));
+    semaphores.put("stat_A2", new Semaphore(1));
+    semaphores.put("stat_B1", new Semaphore(1));
+    semaphores.put("stat_B2", new Semaphore(1));
   }
 }
--- Lab1/trainspotting/src/Train.java
+++ Lab1/trainspotting/src/Train.java
@@ -1,23 +1,37 @@
+import TSim.CommandException;
 import TSim.SensorEvent;
 import TSim.TSimInterface;
 
+import java.util.HashMap;
+import java.util.concurrent.Semaphore;
+
 public class Train extends Thread {
     private TSimInterface tsi = TSimInterface.getInstance();
+    private HashMap<String, Semaphore> semaphores;
+    enum Direction { A_B, B_A, Unknown }
 
     private int id;
     private int speed;
+    private String currentSem;
+    private String lastSem = "NULL";
+    private Direction direction;
+    private boolean running = false;
 
-    public Train (int id, int speed)  {
+    public Train (int id, int speed, HashMap<String, Semaphore> semaphores, String currentSem)  {
         this.id = id;
         this.speed = speed;
+        this.semaphores = semaphores;
+        this.currentSem = currentSem;
     }
 
     @Override
     public void run () {
         try {
-            tsi.setSpeed(this.id, this.speed);
-            //while (true)
-            //  processTrain (tsi.getSensor(this.id));
+            initalDir();
+            startProtocol ();
+
+            while (true)
+                processTrain (tsi.getSensor(this.id));
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -26,8 +40,111 @@ public class Train extends Thread {
     }
 
 
-    public static void processTrain (SensorEvent event) {
-        throw new UnsupportedOperationException ("Not implemented, yet");
+
+
+    public void processTrain (SensorEvent event) throws CommandException, InterruptedException {
+            int x = event.getXpos();
+            int y = event.getYpos();
+
+        if (((x == 14 && y == 3) || (x == 14 && y == 5)) && event.getStatus() == SensorEvent.ACTIVE) {
+            if (this.direction == Direction.B_A) {
+            stopProtocol();
+            }
+            else
+                semRel(this.currentSem);
+        }
+
+        if (((x == 14 && y == 11) || (x == 14 && y == 13)) && event.getStatus() == SensorEvent.ACTIVE) {
+            if (this.direction == Direction.A_B) {
+            stopProtocol();
+            }
+            else
+                semRel(this.currentSem);
+        }
+
+        if ((x == 2 && y == 9) && event.getStatus() == SensorEvent.ACTIVE) {
+            if (this.direction == Direction.A_B) {
+                if (semAcq("stat_B1"))
+                    tsi.setSwitch(3, 11, 1);
+                else {
+                    semAcq("stat_B2");
+                    tsi.setSwitch(3, 11, 0);
+                }
+            }
+        }
+
+        if ((x == 17 && y == 9) && event.getStatus() == SensorEvent.ACTIVE) {
+            if (this.direction == Direction.B_A) {
+                if (semAcq("stat_A1"))
+                    tsi.setSwitch(17, 7, 0);
+                else {
+                    semAcq("stat_A2");
+                    tsi.setSwitch(17, 7, 1);
+                }
+            }
+        }
+    }
+
+
+    public void startProtocol () throws CommandException {
+        semAcq(this.currentSem);
+
+        tsi.setSpeed(this.id, this.speed);
+        System.out.println("" + this.id + " Started" );
+
+    }
+    public void stopProtocol () throws CommandException, InterruptedException {
+
+        tsi.setSpeed(this.id, 0);
+        this.running = false;
+
+        System.out.println("" + this.id + " Stopped" );
+
+        int delay = 1000 + (20 * Math.abs(this.speed));
+        this.speed = this.speed * -1;
+
+        sleep(delay);
+        //semRel(this.currentSem);
+        switchDir();
+        startProtocol();
+
+    }
+    public void initalDir () {
+        if ( (this.currentSem.equals("stat_A1")) || (this.currentSem.equals("stat_A2")) ) {
+            this.direction = Direction.A_B;
+        }
+        else {
+            this.direction = Direction.B_A;
+        }
+    }
+    public void switchDir () {
+        if (this.direction == Direction.A_B)
+            this.direction = Direction.B_A;
+        else
+            this.direction = Direction.A_B;
+    }
+
+    public void quickStart () throws CommandException { tsi.setSpeed(this.id, this.speed); this.running = true;}
+    public void quickStop () throws CommandException { tsi.setSpeed(this.id, 0); this.running = false;}
+
+    private boolean semAcq (String semName) {
+        if (semaphores.get(semName).tryAcquire()) {
+            this.lastSem = currentSem;
+            this.currentSem = semName;
+            System.out.println("" + semName + " acquired");
+            return true;
+        }
+        else
+            return false;
+    }
+    private boolean semRel (String semName) {
+        Semaphore sem = semaphores.get(semName);
+        if (sem.availablePermits() > 0)
+            return false;
+        else
+            sem.release();
+        System.out.println("" + semName + " released");
+        return true;
     }
 
 }
