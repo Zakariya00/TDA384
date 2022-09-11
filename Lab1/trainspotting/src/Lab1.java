6fb7b9fadbd6fecc62748d0b5676c76af29b5415
zakariya00 <zakaria415.zo@gmail.com>
2022-09-11 21:26:37 +0200

    Done!

--- Lab1/trainspotting/src/Lab1.java
+++ Lab1/trainspotting/src/Lab1.java
@@ -15,23 +15,16 @@ public class Lab1 {
       Train train1 = new Train(1, speed1, semaphores, "stat_A1");
       Train train2 = new Train(2, speed2, semaphores, "stat_B1");
 
+      //tsi.setSwitch(17,7,0);   //Points up with 0
+      //tsi.setSwitch(15,9,0);   //Points up with 0
+      //tsi.setSwitch(4, 9, 1);  //Points up with 1
+      //tsi.setSwitch(3, 11, 1); //Points up with 1
+
       train1.start();
       train2.start();
 
-      //tsi.setSwitch(17,7,0); //Points up with 0
-      //tsi.setSwitch(15,9,0); //Points up with 0
-      //tsi.setSwitch(3, 11, 0);
-
-      //tsi.setSwitch(17,7,1); //Points up with 0
-      //tsi.setSwitch(15,9,0); //Points up with 0
-      //tsi.setSwitch(3, 11, 1);
-
-
-
-      //tsi.setSwitch(4,9,1); //Points up with 1
-      //tsi.setSwitch(3,11,1); //Points up with 1
-
-      //tsi.setSpeed(2,speed2);
+      train1.join();
+      train2.join();
     }
     catch (Exception e) {
       e.printStackTrace();    // or only e.getMessage() for the error
@@ -39,9 +32,9 @@ public class Lab1 {
     }
   }
   public void putSemaphores() {
-    semaphores.put("stat_A1", new Semaphore(0));
+    semaphores.put("stat_A1", new Semaphore(1));
     semaphores.put("stat_A2", new Semaphore(1));
-    semaphores.put("stat_B1", new Semaphore(0));
+    semaphores.put("stat_B1", new Semaphore(1));
     semaphores.put("stat_B2", new Semaphore(1));
 
     semaphores.put("crossing", new Semaphore(1));
--- Lab1/trainspotting/src/Train.java
+++ Lab1/trainspotting/src/Train.java
@@ -2,34 +2,34 @@ import TSim.CommandException;
 import TSim.SensorEvent;
 import TSim.TSimInterface;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.Semaphore;
 
 public class Train extends Thread {
     private TSimInterface tsi = TSimInterface.getInstance();
     private HashMap<String, Semaphore> semaphores;
+    private ArrayList<Semaphore> mySemaphores = new ArrayList<>();
     enum Direction { A_B, B_A}
 
     private int id;
     private int speed;
-    private String currentSem;
-    private String lastSem;
+    private String startStation;
     private Direction direction;
-    private boolean upperTrack;
-    private boolean waiting = false;
+    private int lastX;
+    private int lastY;
 
-    public Train (int id, int speed, HashMap<String, Semaphore> semaphores, String currentSem)  {
+    public Train (int id, int speed, HashMap<String, Semaphore> semaphores, String startStation)  {
         this.id = id;
         this.speed = speed;
+        this.startStation = startStation;
         this.semaphores = semaphores;
-        this.currentSem = currentSem;
-        this.lastSem = currentSem;
-        this.upperTrack = true;
     }
 
     @Override
     public void run () {
         try {
+            semAcq(startStation);
             initalDir();
             startProtocol ();
 
@@ -47,120 +47,171 @@ public class Train extends Thread {
         int y = event.getYpos();
 
         if (event.getStatus() == SensorEvent.ACTIVE) {
+            lastX = x;
+            lastY = y;
 
             // From A to B
             if (direction == Direction.A_B) {
                 if ((x == 6 && y == 7) || (x == 8 && y == 5)) {  // Acquire crossing
-                    quickStop();
-                    while (!semAcq("crossing")) sleep (250);
-                    quickStart();
+                    //singleTrack("crossing");
+                    theCrossing();
                 }
 
-                if ((x == 12 && y == 7) || (x == 12 && y == 8)) { // Release crossing and Acquire track_A
+                if ((x == 12 && y == 7) || (x == 12 && y == 8)) { // Release crossing & Acquire track_A
                     semRel("crossing");
-                    quickStop();
-                    while (!semAcq("track_A")) sleep(250);
-
-                    if (upperTrack) tsi.setSwitch(17, 7, 0);
-                    else if (!upperTrack) tsi.setSwitch(17, 7, 1);
-                    quickStart();
+                    singleTrack("track_A");
                 }
 
-                if (x == 19 && y == 8) { // Release Higher Stations and Acquire next Track
-                    if (upperTrack) semRel("stat_A1");
-                    else if (!upperTrack) semRel("stat_A2");
-                    if (!semAcq("stat_B1")) semAcq("stat_B2");
-
-                    if (upperTrack) tsi.setSwitch(15, 9, 0);
-                    else if (!upperTrack) tsi.setSwitch(15, 9, 1);
+                if (x == 19 && y == 8) { // Release Higher Stations & Acquire next Track [Double Track]
+                    semRel("stat_A1"); semRel("stat_A2");
+                    multiTrack("track_B1", "track_B2");
                 }
 
-                if ((x == 9 && y == 9) || (x == 9 && y == 10)) { // Acquire track_B
+                if ((x == 9 && y == 9) || (x == 9 && y == 10)) { // Release track_A track & Acquire track_B
                     semRel("track_A");
-                    quickStop();
-                    while (!semAcq("track_B")) sleep(250);
-
-                    if (upperTrack) tsi.setSwitch(4, 9, 1);
-                    else if (!upperTrack) tsi.setSwitch(4, 9, 0);
-                    quickStart();
+                    singleTrack("track_B");
                 }
 
-                if ((x == 1 && y == 10)) { // Acquire Lower Stations
-                    if (upperTrack) semRel("track_B1");
-                    else if (!upperTrack) semRel("track_B2");
-                    if(!semAcq("stat_B1")) semAcq("stat_B2");
-
-                    if (upperTrack) tsi.setSwitch(3, 11, 1);
-                    else if (!upperTrack) tsi.setSwitch(3, 11, 0);
+                if ((x == 1 && y == 10)) { //  Release double track & Acquire Lower Stations
+                    semRel("track_B1"); semRel("track_B2");
+                    multiTrack("stat_B1", "stat_B2");
                 }
 
-                if ((x== 4 && y == 13) || (x == 5 && y == 11)) semRel("track_B");
+                if ((x== 4 && y == 13) || (x == 5 && y == 11)) { // Relaease track_B
+                    semRel("track_B");
+                }
 
-                if ((x == 14 && y == 11) || (x == 14 && y == 13)) // Stopping at Lower Stations
+                if ((x == 14 && y == 11) || (x == 14 && y == 13)) {  // Stop at Lower Stations & stop protocol
                     stopProtocol();
+                }
 
             }
 
             // From B to A
             if (direction == Direction.B_A) {
                 if ((x == 5 && y == 11) || (x == 4 && y == 13)) {   //Acquire track_B
-                    quickStop();
-                    while (!semAcq("track_B")) sleep(250);
-
-                    if (upperTrack) tsi.setSwitch(3, 11, 1);
-                    else if (!upperTrack) tsi.setSwitch(3, 11, 0);
-                    quickStart();
+                    singleTrack("track_B");
                 }
 
-                if (x == 1 && y == 10) { // Release Lower Stations and Acquire next Track
-                    if (upperTrack) semRel("stat_B1");
-                    else if (!upperTrack) semRel("stat_B2");
-                    if (!semAcq("track_B1")) semAcq("track_B2");
-
-                    if (upperTrack) tsi.setSwitch(4, 9, 1);
-                    else if (!upperTrack) tsi.setSwitch(4, 9, 0);
+                if (x == 1 && y == 10) { // Release Lower Stations & Acquire next Track [Double Track]
+                    semRel("stat_B1"); semRel("stat_B2");
+                    multiTrack("track_B1", "track_B2");
                 }
 
-                if ((x == 9 && y == 9) || (x == 9 && y == 10)) { // Acquire track_A
+                if ((x == 9 && y == 9) || (x == 9 && y == 10)) { // Release track_B & Acquire track_A
                     semRel("track_B");
-                    quickStop();
-                    while (!semAcq("track_A")) sleep(250);
-
-                    if (upperTrack) tsi.setSwitch(15, 9, 0);
-                    else if (!upperTrack) tsi.setSwitch(15, 9, 1);
-                    quickStart();
+                    singleTrack("track_A");
                 }
 
-                if ((x == 19 && y == 8)) {  // Acquire Higher Stations
-                    if (upperTrack) semRel("track_B1");
-                    else if (!upperTrack) semRel("track_B2");
-                    if (!semAcq("stat_A1")) semAcq("stat_A2");
-
-                    if (upperTrack) tsi.setSwitch(17, 7, 0);
-                    else if (!upperTrack) tsi.setSwitch(17, 7, 1);
+                if ((x == 19 && y == 8)) {  // Release double track & Acquire Higher Stations
+                    semRel("track_B1"); semRel("track_B2");
+                    multiTrack("stat_A1", "stat_A2");
                 }
 
-                if ((x == 12 && y == 7) || (x == 12 && y == 8)) {  // Acquire crossing
+                if ((x == 12 && y == 7) || (x == 12 && y == 8)) {  // Release track_A & Acquire crossing
                     semRel("track_A");
-                    quickStop();
-                    while (!semAcq("crossing")) sleep (250);
-                    quickStart();
+                    //singleTrack("crossing");
+                    theCrossing();
                 }
 
-                if ((x == 6 && y == 7) || (x == 8 && y == 5))  // Release crossing
+                if ((x == 6 && y == 7) || (x == 8 && y == 5)) {  // Release crossing
                     semRel("crossing");
+                }
 
-                if ((x == 14 && y == 3) || (x == 14 && y == 5)) // Stopping at Higher Stations
+                if ((x == 14 && y == 3) || (x == 14 && y == 5)) { // Stop at Higher Stations & stop protocol
                     stopProtocol();
+                }
+
 
             }
         }
     }
 
+    private void switchSet (String semName) throws CommandException {
+        if (direction == Direction.A_B)
+            switch4AB(semName);
+        else
+            switch4BA(semName);
+    }
+    private void switch4AB (String semName) throws CommandException {
+        if (semName.equals("track_A")) {
+        if (lastX == 12 && lastY == 7)
+            tsi.setSwitch(17, 7, 0);
+        if (lastX == 12 && lastY == 8)
+            tsi.setSwitch(17, 7, 1);
+        }
+
+        if (semName.equals("track_B1"))
+            tsi.setSwitch(15, 9, 0);
+        if (semName.equals("track_B2"))
+            tsi.setSwitch(15, 9, 1);
+
+        if (semName.equals("track_B")) {
+            if (lastX == 9 && lastY == 9)
+                tsi.setSwitch(4, 9, 1);
+            if (lastX == 9 && lastY == 10)
+                tsi.setSwitch(4, 9, 0);
+        }
+
+        if (semName.equals("stat_B1"))
+            tsi.setSwitch(3, 11, 1);
+        if (semName.equals("stat_B2"))
+            tsi.setSwitch(3, 11, 0);
+
+    }
+    private void switch4BA (String semName) throws CommandException {
+        if (semName.equals("track_B")) {
+            if (lastX == 5 && lastY == 11)
+                tsi.setSwitch(3, 11, 1);
+            if (lastX == 4 && lastY == 13)
+                tsi.setSwitch(3, 11, 0);
+        }
+
+        if (semName.equals("track_B1"))
+            tsi.setSwitch(4, 9, 1);
+        if (semName.equals("track_B2"))
+            tsi.setSwitch(4, 9, 0);
+
+        if (semName.equals("track_A")) {
+            if (lastX == 9 && lastY == 9)
+                tsi.setSwitch(15, 9, 0);
+            if (lastX == 9 && lastY == 10)
+                tsi.setSwitch(15, 9, 1);
+        }
+
+        if (semName.equals("stat_A1"))
+            tsi.setSwitch(17, 7, 0);
+        if (semName.equals("stat_A2"))
+            tsi.setSwitch(17, 7, 1);
+    }
+
+    private String multiTrack (String semName1, String semName2) throws CommandException {
+        quickStop();
+        if (!semAcq(semName1)) {
+            semAcq(semName2);
+            switchSet(semName2);
+            quickStart();
+            return semName2;
+        }
+        switchSet(semName1);
+        quickStart();
+        return semName1;
+    }
+    private void singleTrack (String semName) throws InterruptedException, CommandException {
+        quickStop();
+        while (!semAcq(semName))
+            sleep(250);
+        switchSet(semName);
+        quickStart();
+    }
+    private void theCrossing () throws CommandException, InterruptedException {
+        quickStop();
+        while (!semAcq("crossing"))
+            sleep(250);
+        quickStart();
+    }
 
     public void startProtocol () throws CommandException {
-        //semAcq(this.currentSem);
-
         tsi.setSpeed(this.id, this.speed);
         System.out.println("" + this.id + " Started" );
     }
@@ -173,13 +224,11 @@ public class Train extends Thread {
         sleep(delay);
 
         switchDir();
-
-        //semRel(this.currentSem);
         startProtocol();
     }
 
     public void initalDir () {
-        if ( (this.currentSem.equals("stat_A1")) || (this.currentSem.equals("stat_A2")) ) {
+        if ( (this.startStation.equals("stat_A1")) || (this.startStation.equals("stat_A2")) ) {
             this.direction = Direction.A_B;
         }
         else {
@@ -193,20 +242,13 @@ public class Train extends Thread {
             this.direction = Direction.A_B;
     }
 
-    public void quickStart () throws CommandException { tsi.setSpeed(this.id, this.speed); waiting = false;}
-    public void quickStop () throws CommandException { tsi.setSpeed(this.id, 0); waiting = true;}
+    public void quickStart () throws CommandException { tsi.setSpeed(this.id, this.speed);}
+    public void quickStop () throws CommandException { tsi.setSpeed(this.id, 0);}
 
     private boolean semAcq (String semName) {
         if (semaphores.get(semName).tryAcquire()) {
-            if (!semName.equals("crossing")) {
-                if (semName.equals("stat_A1") || semName.equals("track_B1") || semName.equals("stat_B1"))
-                    upperTrack = true;
-                else if (semName.equals("stat_A2") || semName.equals("track_B2") || semName.equals("stat_B2"))
-                    upperTrack = false;
-                System.out.println(upperTrack);
-            }
-
             System.out.println("" + semName + " acquired");
+            mySemaphores.add(semaphores.get(semName)); // test
             return true;
         }
         else
@@ -217,100 +259,12 @@ public class Train extends Thread {
         if (sem.availablePermits() > 0)
             return false;
         else
+        if (!mySemaphores.contains(sem))
+            return false;
             sem.release();
+        mySemaphores.remove(sem); // test
         System.out.println("" + semName + " released");
         return true;
     }
 }
-
-
-   /* public void processTrain (SensorEvent event) throws CommandException, InterruptedException {
-        int x = event.getXpos();
-        int y = event.getYpos();
-
-        if (event.getStatus() == SensorEvent.ACTIVE) {
-
-            //Stopping at Stations
-            if ((((x == 14 && y == 3) || (x == 14 && y == 5)) && direction == Direction.B_A) ||
-                       (((x == 14 && y == 11) || (x == 14 && y == 13)) && direction == Direction.A_B)) {
-                stopProtocol();
-            }
-            //Setting Switches A_B
-            if (direction == Direction.A_B) {
-                if ((x == 12 && y == 7) || (x == 12 && y == 8) || (x == 19 && y == 8) || (x == 9 && y == 9) || (x == 9 && y == 10) || (x == 1 && y == 10)) {
-                    changeSwitch(x, y, direction);
-                }}
-
-            //Setting Switches B_A
-            if (direction == Direction.B_A) {
-                if ((x == 14 && y == 13) || (5 == 12 && y == 11) || (x == 1 && y == 10) || (x == 9 && y == 9) || (x == 9 && y == 10) || (x == 19 && y == 8)) {
-                    changeSwitch(x, y, direction);
-                }}
-
-            //Crossing Sections
-
-        }
-    }
-
-    private void changeSwitch (int x, int y, Direction dir) {
-
-        }
-    }*/
-
-   /* private boolean nextTrack (String trackSem) throws CommandException, InterruptedException {
-        quickStop();
-        if (semAcq(trackSem)) {
-            semRel(this.lastSem);
-            switch4Track();
-            quickStart();
-            System.out.println("Track Acquired by Train " + this.id);
-            return true;
-        }
-        System.out.println("Wait for Track Train " + this.id);
-        return false;
-    }
-    private void switch4Track () throws CommandException {
-        // Direction B to A
-        if (lastSem.equals("stat_B1") && currentSem.equals("track_B"))
-            tsi.setSwitch(3, 11, 1);
-        if (lastSem.equals("stat_B2") && currentSem.equals("track_B"))
-            tsi.setSwitch(3, 11, 0);
-
-        if (lastSem.equals("track_B") && currentSem.equals("track_B1"))
-            tsi.setSwitch(4, 9, 1);
-        if (lastSem.equals("track_B") && currentSem.equals("track_B2"))
-            tsi.setSwitch(4, 9, 0);
-
-        if (lastSem.equals("track_B1") && currentSem.equals("track_A"))
-            tsi.setSwitch(15, 9, 0);
-        if (lastSem.equals("track_B2") && currentSem.equals("track_A"))
-            tsi.setSwitch(15, 9, 1);
-
-        if (lastSem.equals("track_A") && currentSem.equals("stat_A1"))
-            tsi.setSwitch(17, 7, 0);
-        if (lastSem.equals("track_A") && currentSem.equals("stat_A2"))
-            tsi.setSwitch(17, 7, 1);
-
-        // Direction A to B
-        if (lastSem.equals("stat_A1") && currentSem.equals("track_A")) // ---
-            tsi.setSwitch(17, 7, 0);
-        if (lastSem.equals("stat_A2") && currentSem.equals("track_A"))
-            tsi.setSwitch(17, 7, 1);
-
-        if (lastSem.equals("track_A") && currentSem.equals("track_B1"))
-            tsi.setSwitch(15, 9, 0);
-        if (lastSem.equals("track_A") && currentSem.equals("track_B2"))
-            tsi.setSwitch(15, 9, 1);
-
-        if (lastSem.equals("track_B1") && currentSem.equals("track_B"))
-            tsi.setSwitch(4, 9, 1);
-        if (lastSem.equals("track_B2") && currentSem.equals("track_B"))
-            tsi.setSwitch(4, 9, 0);
-
-        if (lastSem.equals("track_B") && currentSem.equals("stat_B1"))
-            tsi.setSwitch(3, 11, 1);
-        if (lastSem.equals("track_B") && currentSem.equals("stat_B2"))
-            tsi.setSwitch(3, 11, 0);
-    }*/
-
 
